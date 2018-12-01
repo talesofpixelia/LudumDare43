a78c2b7fbbca5cdee0f59a760962475d3c6d37b4
jer2spy <matthias.mauriere@gmail.com>
2018-12-01 14:57:34 +0100

    Ia for Grind

---
+++ Assets/Scripts/FiniteStateMachine/StateMachineGrind.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.Scripts.FiniteStateMachine
+{
+    public class StateMachineGrind : MonoBehaviour
+    {
+        private int ResponceTimeMillisecond { get; set; }
+        private int Percentage { get; set; }
+
+        private string[] ResultWin = new string[] { "a", "b", "x", "y" };
+
+        public StateMachineGrind(int responceTime, int percentage)
+        {
+            this.ResponceTimeMillisecond = responceTime;
+            this.Percentage = percentage;
+        }
+
+        public string PlayerController(string grindCorect)
+        {
+            DateTime start = DateTime.Now;
+            string result = grindCorect;
+            if (!Win())
+            {
+                do
+                {
+                    System.Random rnd = new System.Random();
+                    result = ResultWin[rnd.Next(0,3)];
+
+                } while (result == grindCorect);
+            }
+
+            TimeSpan dur = DateTime.Now - start;
+
+            return result;
+        }
+
+        class Items
+        {
+            public double Probability { get; set; }
+            public bool Item { get; set; }
+        }
+
+        private bool Win()
+        {
+            bool win = false;
+            var elements = new List<Items>
+            {
+                new Items {Probability = this.Percentage / 100.0, Item = true},
+                new Items {Probability = (100.0 - this.Percentage ) / 100.0, Item = false}
+            };
+
+            System.Random r = new System.Random();
+            double diceRoll = r.NextDouble();
+
+            double cumulative = 0.0;
+            for (int i = 0; i < elements.Count; i++)
+            {
+                cumulative += elements[i].Probability;
+                if (diceRoll < cumulative)
+                {
+                    win = elements[i].Item;
+                    break;
+                }
+            }
+            return win;
+        }
+      
+
+    }
+}
+
+
+
