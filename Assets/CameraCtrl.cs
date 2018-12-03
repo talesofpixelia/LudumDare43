c98f24e3b952f16048896e391116f151b3e14daa
Wojciech Zyla <woj.zyla+bit@gmail.com>
2018-12-03 17:44:35 +0100

    Grind scene switch to brawl

---
+++ Assets/CameraCtrl.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraCtrl : MonoBehaviour {
+
+    public GrindPlayerManager PlayerManager;
+
+	public void ZoomInFinished()
+    {
+        PlayerManager.ShowWeapons();
+    }
+}
