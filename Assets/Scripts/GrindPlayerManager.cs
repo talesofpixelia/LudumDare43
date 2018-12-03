23498601240f5211d66dbd51740fd5e2a353c6e7
Wojciech Zyla <woj.zyla+bit@gmail.com>
2018-12-03 14:48:56 +0100

    Grind particle effects on button push

---
+++ Assets/Scripts/GrindPlayerManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class GrindPlayerManager : MonoBehaviour {
+
+    public CommandController commandController;
+    public Player[] Players = new Player[4];
+    
+    // Use this for initialization
+    void Start () {
+		if(commandController)
+        {
+             Players.CopyTo(commandController.Players,0);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+        //PersonalScore.GetComponent<TextMeshPro>().text = Players[0].Score.ToString("D5");
+    }
+}
