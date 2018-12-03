913bd56003eb596de67d47980e7b469ccfd1af4a
talesofpixelia <42294009+talesofpixelia@users.noreply.github.com>
2018-12-03 21:45:09 +0100

    PPE End Screen Done
     End Screen Done

---
+++ Assets/Scripts/EndController.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndController : MonoBehaviour {
+
+    public float Duration = 5;
+
+    public GameObject[] particles;
+
+    public GameObject congratz;
+    public GameObject sacrifice;
+
+    // Use this for initialization
+    void Start () {
+
+        congratz.SetActive(true);
+        sacrifice.SetActive(false);
+    }
+    void Awake()
+    {
+
+        congratz.SetActive(true);
+        sacrifice.SetActive(false);
+    }
+
+
+    // Update is called once per frame
+    void Update () {
+
+        StartCoroutine(Sleep());
+	}
+
+    public IEnumerator Sleep()
+    {
+        for (int i = 0; i < Duration; i++)
+        {
+            Debug.Log(i);
+
+            if (i == 1 || i == 4)
+            {
+                foreach (var particle in particles)
+                {
+                    particle.SetActive(true);
+                }
+            }
+
+            if (i == 4)
+            {
+                congratz.SetActive(false);
+                sacrifice.SetActive(true);
+            }
+            yield return new WaitForSeconds(1);
+        }
+
+        SceneManager.LoadScene(0);
+    }
+}
